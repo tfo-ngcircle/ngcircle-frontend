85745c9f29217bc2173be5130d5e294e086fdb9c
Forntoh Thomas <thomasforntoh@gmail.com>
2021-08-10 13:36:03 +0200

    finished seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
-import Head from "next/head";
 import { fetchApi } from "@/lib/api";
 import { GlobalContext } from "./_app";
 import { useContext } from "react";
 import CTA from "@/components/cta";
 import Container from "@/components/container";
+import Seo from "@/components/seo";
 
 export default function Home({ homepage }) {
   const { header } = useContext(GlobalContext);
   return (
-    <Container header={header}>
-      <CTA cta={homepage.CTA} style={{ backgroundColor: "#000" }} />
-    </Container>
+    <>
+      <Seo seo={homepage.seo} />
+      <Container header={header}>
+        <CTA cta={homepage.CTA} style={{ backgroundColor: "#000" }} />
+      </Container>
+    </>
   );
 }
 
