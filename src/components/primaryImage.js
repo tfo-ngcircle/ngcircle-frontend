025cf1791ba94b3ddbc67331f63c3c8b11c18624
Forntoh Thomas <thomasforntoh@gmail.com>
2021-08-11 18:28:41 +0200

    finished primary image

--- src/components/primaryImage.js
+++ src/components/primaryImage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { getStrapiMedia } from "@/lib/media";
 import MyImage from "./image";
 import styles from "../styles/components/PrimaryImage.module.scss";
+import ReactPlayer from "react-player";
 
 const PrimaryImage = ({ primaryImage }) => {
   const left = primaryImage.leftContent;
@@ -11,6 +12,7 @@ const PrimaryImage = ({ primaryImage }) => {
       className={styles.primaryImage}
       style={{
         backgroundImage: `url(${getStrapiMedia(primaryImage.backgroundImage)})`,
+        minHeight: `${Math.min(692, primaryImage.height)}px`,
       }}
     >
       {left && renderMedia(left)}
@@ -25,5 +27,22 @@ export default PrimaryImage;
 function renderMedia(media) {
   if (media.mime.includes("image")) {
     return <MyImage image={media} />;
+  } else if (media.mime.includes("video")) {
+    return (
+      <ReactPlayer
+        url={getStrapiMedia(media)}
+        controls
+        loop
+        muted
+        playing
+        config={{
+          file: {
+            attributes: {
+              controlsList: "nodownload",
+            },
+          },
+        }}
+      />
+    );
   }
 }
