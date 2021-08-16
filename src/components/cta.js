26951cffdae8db988911d62de2ebf70099359211
Forntoh Thomas <thomasforntoh@gmail.com>
2021-08-16 19:50:33 +0200

    fix cta

--- src/components/cta.js
+++ src/components/cta.js
@@ -5,11 +5,13 @@ import Link from "next/link";
 const CTA = ({ cta, style }) => {
   return (
     <section className="border-b">
-      <div className="sm:grid sm:grid-cols-4 sm:container items-center overflow-hidden">
-        <h1 className="col-span-3 py-14 xl:pr-10">{cta.description}</h1>
+      <div className="sm:grid sm:grid-cols-4 sm:container items-center overflow">
+        <h1 className="col-span-3 py-14 xl:pr-10 overflow-ellipsis overflow-hidden">
+          {cta.description}
+        </h1>
         <div
           style={style}
-          className="flex min-h-full md:w-[208px] lg:w-[316px] xl:w-[440px] 2xl:w-[440px] pt-8 pb-8 text-gray-200 hover:text-white space-x-6"
+          className="flex min-h-full md:w-[208px] lg:w-[316px] xl:w-[440px] 2xl:w-[440px] py-8 text-gray-200 hover:text-white space-x-6"
         >
           <Image
             src="/arrow.svg"
