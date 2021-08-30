b4eeb8a54356282779ef3b0777db42ca4937ebef
Forntoh Thomas <thomasforntoh@gmail.com>
2021-08-30 12:52:35 +0200

    added slider

--- src/components/primaryImage.js
+++ src/components/primaryImage.js
@@ -1,31 +1,79 @@
-import React from "react";
 import { getStrapiMedia } from "@/lib/media";
+import { Parallax } from "react-parallax";
 import MyImage from "./image";
 import ReactPlayer from "react-player";
-import { Parallax } from "react-parallax";
+import AwesomeSlider from "react-awesome-slider";
+import styled from "styled-components";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import "react-awesome-slider/dist/styles.css";
+import ReactMarkdown from "react-markdown";
+import useCurrentBreakpoint from "@/lib/tw";
 
-const PrimaryImage = ({ primaryImage }) => {
-  const left = primaryImage.leftMedia;
-  const right = primaryImage.rightMedia;
-  const minHeight = Math.min(692, primaryImage.backgroundImage.height);
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
-  return (
-    <Parallax
-      bgImage={getStrapiMedia(primaryImage.backgroundImage)}
-      strength={300}
-      bgClassName="max-w-none"
-    >
-      <div
-        className="container flex flex-wrap justify-between"
-        style={{ minHeight: `${minHeight}px` }}
-      >
-        {left && renderMedia(left)}
-        <div></div>
-        {right && renderMedia(right)}
-      </div>
-    </Parallax>
-  );
-};
+function PrimaryImage({ primaryImage }) {
+  const breakpoint = useCurrentBreakpoint();
+  var heightPercentage = "";
+
+  if (breakpoint === "xl" || breakpoint === "2xl") heightPercentage = "36%";
+  else heightPercentage = "80%";
+
+  switch (primaryImage.__component) {
+    case "shared.slider":
+      return (
+        <Slider heightPercentage={heightPercentage} play={true} interval={6000}>
+          {primaryImage.images.map((image, index) => (
+            <div
+              data-src={getStrapiMedia(image)}
+              key={image.id}
+              className="translate-y-0 text-black bg-white bg-opacity-80 p-8 shadow-lg rounded-sm max-w-2xl"
+            >
+              <ReactMarkdown>
+                {primaryImage.description[index].md}
+              </ReactMarkdown>
+            </div>
+          ))}
+        </Slider>
+      );
+    case "landing.primary-image":
+      const left = primaryImage.leftMedia;
+      const right = primaryImage.rightMedia;
+      const minHeight = Math.min(692, primaryImage.backgroundImage.height);
+
+      return (
+        <Parallax
+          bgImage={getStrapiMedia(primaryImage.backgroundImage)}
+          strength={300}
+          bgClassName="max-w-none"
+        >
+          <div
+            className="container flex flex-wrap justify-between"
+            style={{ minHeight: `${minHeight}px` }}
+          >
+            {left && renderMedia(left)}
+            <div></div>
+            {right && renderMedia(right)}
+          </div>
+        </Parallax>
+      );
+  }
+}
+
+const Slider = styled(AutoplaySlider)`
+  --slider-height-percentage: ${(props) => props.heightPercentage};
+  --slider-transition-duration: 700ms;
+  --organic-arrow-thickness: 4px;
+  --organic-arrow-border-radius: 0px;
+  --organic-arrow-height: 20px;
+  --organic-arrow-color: #c00000;
+  --control-button-width: 15%;
+  --control-button-height: 25%;
+  --control-button-background: transparent;
+  --control-bullet-color: #65221d85;
+  --control-bullet-active-color: #65221d;
+  --loader-bar-color: #65221d;
+  --loader-bar-height: 6px;
+`;
 
 export default PrimaryImage;
 
