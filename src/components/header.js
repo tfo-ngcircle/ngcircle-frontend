c9d0107a2b8947ad3096f4aabcd4ac2c2db77293
Forntoh Thomas <thomasforntoh@gmail.com>
2021-09-02 14:40:28 +0200

    made header sticky + added shadow

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,30 @@
-import React from "react";
 import MyImage from "./image";
 import MyLink from "./link";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import React, { useState } from "react";
+import { useScrollPosition } from "@/lib/useScrollPosition";
 
 const Header = ({ logo, items }) => {
   const router = useRouter();
+
+  const [shadow, setShadow] = useState(false);
+
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      const isShow = currPos.y < 0;
+      if (isShow !== shadow) setShadow(isShow);
+    },
+    [shadow]
+  );
+
   return (
-    <div className="border-b">
-      <header className="container xl:flex items-center justify-between py-4">
+    <div
+      className={`border-b bg-white sticky transition-shadow duration-500 ease-in-out left-0 right-0 top-0 z-50 ${
+        shadow ? "shadow-md" : ""
+      } `}
+    >
+      <header className={`container xl:flex items-center justify-between py-4`}>
         <Link href="/">
           <a>
             <MyImage image={logo} layout="intrinsic" />
