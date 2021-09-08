6d892861cec5cdfa2daee4244ade3e391122c466
Forntoh Thomas <thomasforntoh@gmail.com>
2021-09-08 11:19:44 +0200

    finished contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,11 @@
 import { fetchApi, postApi } from "@/lib/api";
 import { GlobalContext } from "./_app";
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import CTA from "@/components/cta";
 import Container from "@/components/container";
 import Seo from "@/components/seo";
 import PrimaryImage from "@/components/primaryImage";
 import Content from "@/components/content";
-import { AtSymbolIcon, IdentificationIcon } from "@heroicons/react/solid";
 import ShortInput from "@/components/shortInput";
 import TextArea from "@/components/textArea";
 import Checkbox from "@/components/checkbox";
@@ -24,28 +23,37 @@ function Contact({ contact }) {
   const router = useRouter();
 
   const [captchaValid, setCaptchaValid] = useState(false);
+  const captchaRef = useRef(null);
 
   // form validation rules
   const validationSchema = Yup.object().shape({
     name: Yup.string().min(3).max(64),
     email: Yup.string().email().required(),
-    message: Yup.string().min(3).max(300),
-    privacy: Yup.bool().oneOf([true], "You must agree to the privacy policy"),
+    message: Yup.string().min(32).max(300),
+    privacy: Yup.bool().oneOf([true]),
   });
 
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting, isDirty, isValid, errors },
+    reset,
+    formState: { isDirty, isValid, errors },
   } = useForm({
     mode: "onChange",
     resolver: yupResolver(validationSchema),
   });
 
   const onSubmit = (data) =>
-    postApi("/messages", data).then((res) => setIsOpen(true));
+    postApi("/messages", data).then(() => {
+      setIsOpen(true);
+      reset();
+      captchaRef.current.resetCaptcha();
+    });
 
   let [isOpen, setIsOpen] = useState(false);
+
+  const privacy = contact.form.find((value) => value.ref === "privacy");
+  const submit = contact.form.find((value) => value.type === "submit");
 
   return (
     <>
@@ -59,37 +67,48 @@ function Contact({ contact }) {
           className="container pb-14 space-y-6"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <ShortInput
-            id="name"
-            label="Name"
-            placeholder="John Doe"
-            icon={<IdentificationIcon className="h-5 w-5" aria-hidden="true" />}
-            {...register("name")}
-            className={errors.name && "bg-red-50 ring-2 ring-red-500"}
-          />
-          <ShortInput
-            id="email"
-            label="Email"
-            placeholder="you@example.com"
-            icon={<AtSymbolIcon className="h-5 w-5" aria-hidden="true" />}
-            {...register("email")}
-            className={errors.email && "bg-red-50 ring-2 ring-red-500"}
-          />
-          <TextArea
-            id="message"
-            label="Message"
-            {...register("message")}
-            className={errors.message && "bg-red-50 ring-2 ring-red-500"}
-          />
+          {contact.form &&
+            contact.form.map((field) => {
+              switch (field.type) {
+                case "text":
+                  return (
+                    <ShortInput
+                      id={field.ref}
+                      label={field.label}
+                      placeholder={field.placeholder}
+                      {...register(field.ref)}
+                      className={
+                        errors[field.ref] && "bg-red-50 ring-2 ring-red-500"
+                      }
+                    />
+                  );
+                case "textArea":
+                  return (
+                    <TextArea
+                      id={field.ref}
+                      label={field.label}
+                      placeholder={field.placeholder}
+                      {...register(field.ref)}
+                      className={
+                        errors[field.ref] && "bg-red-50 ring-2 ring-red-500"
+                      }
+                    />
+                  );
+                default:
+                  break;
+              }
+            })}
           <div className="flex flex-col sm:flex-none sm:grid space-y-3 xl:space-y-0 xl:grid-cols-3 xl:space-x-2">
             <div className="">
-              <Checkbox
-                id="privacy"
-                label="I have read and agree to the Privacy Policy."
-                {...register("privacy")}
-              />
+              {privacy && (
+                <Checkbox
+                  id={privacy.ref}
+                  label={privacy.label}
+                  {...register(privacy.ref)}
+                />
+              )}
               <div className="text-sm text-red-500 px-7 -mt-1">
-                {errors.privacy?.message}
+                {errors.privacy?.message && privacy?.errorMessage}
               </div>
             </div>
 
@@ -98,10 +117,12 @@ function Contact({ contact }) {
               sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_KEY}
               onVerify={() => setCaptchaValid(true)}
               onExpire={() => setCaptchaValid(false)}
+              ref={captchaRef}
             />
             <input
               disabled={captchaValid ? !isDirty || !isValid : true}
               type="submit"
+              value={submit?.label || "Submit"}
               className="disabled:bg-gray-500 shadow xl:col-span-1 px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-primary-dark md:py-4 md:text-lg md:px-10"
             />
           </div>
